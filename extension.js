9f711b0746b8a0c4fc386cff7dad25a40f832619
fthx <thirioux@gmail.com>
2025-09-22 16:18:12 +0200

    Hide overview exiting showapps

--- extension.js
+++ extension.js
@@ -125,8 +125,7 @@ const BottomDock = GObject.registerClass(
             this._dash._dashContainer.track_hover = true;
             this._dash._dashContainer.reactive = true;
 
-            this._dash.showAppsButton.set_toggle_mode(false);
-            this._dash.showAppsButton.connectObject('button-release-event', () => Main.overview.showApps(), this);
+            this._dash.showAppsButton.connectObject('notify::checked', this._onShowAppsButtonChecked.bind(this), this);
 
             if (Main.overview._overview._controls.get_children().includes(this._dash)) {
                 Main.overview._overview._controls.remove_child(this._dash);
@@ -179,6 +178,13 @@ const BottomDock = GObject.registerClass(
                 if (!this._settings.get_boolean('dock-autohide') && !Main.overview.visible)
                     this._hideDash();
             }
+        }
+
+        _onShowAppsButtonChecked() {
+            if (Main.overview.visible)
+                Main.overview.hide();
+            else
+                Main.overview.showApps();
         }
 
         _onOverviewHidden() {
